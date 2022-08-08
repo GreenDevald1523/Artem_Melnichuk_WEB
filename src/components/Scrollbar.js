c1fd7760d52e5e783ed5d7b764242c013774b503
GreenDevald1523 <46032258+GreenDevald1523@users.noreply.github.com>
2022-08-09 01:27:53 +0300

    Add files via upload

---
+++ src/components/Scrollbar.js
@@ -0,0 +1,10 @@
+// let totalHeight = document.querySelector('.section__filmography-items').scrollHeight - document.querySelector('.section__filmography-items').innerHeight
+// let thumb = document.querySelector(".scroll-line-thumb")
+// document.querySelector('.section__filmography-items').onscroll(() => {
+//     let progressHeight = (document.querySelector('.section__filmography-items').pageYOffset / totalHeight) * 100
+//     thumb.style.right = "-" + progressHeight + "%"
+// })
+
+// document.querySelector('.section__filmography-items').addEventListener('scroll', function() {
+//     thumb.style.right = "-" + pageYOffset + 'px';
+// });
